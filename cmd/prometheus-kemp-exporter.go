d31c7204f70c703f7818e5faa89d1075043e9484
Joseph Salisbury <salisbury.joseph@gmail.com>
2016-06-10 10:35:28 +0100

    Add godoc to RootCmd

--- cmd/prometheus-kemp-exporter.go
+++ cmd/prometheus-kemp-exporter.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// RootCmd is the main Cobra Command.
 var RootCmd = &cobra.Command{
 	Use:   "prometheus-kemp-exporter",
 	Short: "prometheus-kemp-exporter exports Kemp statistics to Prometheus",
